082b2d4ea36a5b536f72ac5de6e9932080dc2feb
Dina Benayad-Cherif <dina.benayad.cherif@gmail.com>
2016-12-01 15:34:03 -0500

    instead of writing entire json, just writes text of tweet to the output file
    Signed-off-by: Dina Benayad-Cherif <dina.benayad.cherif@gmail.com>

--- tweet.py
+++ tweet.py
@@ -11,10 +11,13 @@ LANGUAGES = ['en']
 api = twitter.Api(credentials.consumer_key, credentials.consumer_secret, credentials.access_token_key, credentials.access_token_secret)
 
 def main():
-    with open('output.txt', 'a') as f:
+    with open('output2.txt', 'a') as f:
         for line in api.GetStreamFilter(track=['Trump']):
-            f.write(json.dumps(line))
-            f.write('\n')
+            #data = json.dumps(line)
+            if 'text' in line:
+                f.write(json.dumps(line["text"]))
+                f.write('\n')
+            #f.write("User: {user}, Tweet: '{tweet}'".format(user=tweets.user.screen_name, tweet=tweets.text))
 
 
 if __name__ == '__main__':
