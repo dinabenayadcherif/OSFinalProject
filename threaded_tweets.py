5548e75539a0b2ce8b34a28d3dfbca0b6b610309
katherine <cass@case.edu>
2016-12-13 16:07:00 -0500

    refactor threaded tweets

--- threaded_tweets.py
+++ threaded_tweets.py
@@ -8,13 +8,16 @@ import basic_sentiment_analysis
 import signal
 import sys
 
-# Child: begins streaming tweets for LOCATION
+# Child: begins streaming tweets for a LOCATION
 def child(location):
+  # Keep track of local sentiment
   local_meter = 0
+
+  # Used for graceful multi-fork kills in unix
   global killer
   global locations
-  local_meter
-  # put into twitter-api-friendly format
+
+  # put coordinates into twitter-api-friendly format
   coordinates = [locations[location][0], locations[location][1]]
 
   # Open API connection
@@ -24,20 +27,22 @@ def child(location):
       locations[location][4],
       locations[location][5])
 
-  #analytics
-  i = 0
-  start_time = time.time()
+  i = 0 # used for analytics
+  start_time = time.time() # used for analytics
   for line in api.GetStreamFilter(locations=coordinates):
           
     if 'text' in line:
       tweet = json.dumps(line["text"])
       print "\n[location: " + location + "] tweet: " + tweet
 
-      #CRITICAL SECTION
+      ## CRITICAL SECTION ###########
       global global_meter_lock
       with global_meter_lock:
+        # update GLOBAL meter 
         update_global_meter(tweet)
+      ###############################
       
+      # Used for analytics
       i+= 1
       if i >= 20:
         i = 0
@@ -45,15 +50,12 @@ def child(location):
         print "\nLocation: " + location + " processing time: " + str(end_time-start_time) + "for 20 tweets."
         start_time = time.time()
     
-      #write to local meter
+      # write to LOCAL meter
       local_meter = local_meter + basic_sentiment_analysis.get_tweet_score(tweet)
       file_name = location + '_meter.txt'
       wr = open(file_name, 'w')
       wr.write(str(local_meter))  
       wr.close()
-
-
-      #write to global meter ??? TODO
 
       # gracefully kill thread if receive sigkill from terminal
       if killer.kill_now:
@@ -61,24 +63,7 @@ def child(location):
 
   return
 
-#analyzes velocity of hate per 10 seconds
-def meter_maid():
-  time.sleep(10)
-
-  global global_meter_lock
-  with global_meter_lock:
-    meter = 0
-  #CRITICAL SECTION
-    #global meter_lock
-    #  with meter_lock:
-    #    meter = update_global_meter(tweet)
-
-   #    file_name = location + '_meter.txt'
-     #  wr = open(file_name, 'w')
-       #wr.write(meter)  
-
-
-# Parent: creates children, assigning map location (& API keys) from YAML
+# Parent: creates children, assigning map location (& API keys) from locations.yml
 def parent():
   global global_meter
   global_meter = 0
@@ -103,31 +88,36 @@ def parent():
   # code ripped from http://stackoverflow.com/questions/18499497/how-to-process-sigterm-signal-gracefully
   global killer
   killer = GracefulKiller()
-
+  
+  # Sit and let children do work,
+  # Occasionally updating the GLOBAL METER
   while True:
     time.sleep(1)
-
+    
+    ## CRITICAL SECTION ###########
     #update global meter
     global global_meter_lock
     with global_meter_lock:
-      file_name = location + 'Global_meter.txt'
+      file_name = 'Global_meter.txt'
       wr = open(file_name, 'w')
       wr.write(str(global_meter))
       wr.close()  
+    ###############################
 
+    # Kill process gracefully if receive sigkill from user
     if killer.kill_now:
       break
 
+# Updates global meter 
+# ONLY TO BE CALLED IN CRITICAL SECTIONS
 def update_global_meter(tweet):
   global global_meter
   value = basic_sentiment_analysis.get_tweet_score(tweet)
   global_meter = global_meter + value
   return str(global_meter)
 
-def get_sentiment(tweet):
-  value = basic_sentiment_analysis.get_tweet_score(tweet)
-  return value
-
+# code ripped from http://stackoverflow.com/questions/18499497/how-to-process-sigterm-signal-gracefully
+# used because python isn't receiving sigkills from terminal on Ubuntu 16.04
 class GracefulKiller:
   kill_now = False
   def __init__(self):
@@ -137,4 +127,5 @@ class GracefulKiller:
   def exit_gracefully(self,signum, frame):
     self.kill_now = True
 
+# Start!!!!
 parent()
