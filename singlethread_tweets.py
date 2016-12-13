2293ccf5c1486236598ea5c059ea59528e5b454d
Dina Benayad-Cherif <dina.benayad.cherif@gmail.com>
2016-12-13 14:23:56 -0500

    removed unecessary code and improved readability
    Signed-off-by: Dina Benayad-Cherif <dina.benayad.cherif@gmail.com>

--- singlethread_tweets.py
+++ singlethread_tweets.py
@@ -1,4 +1,3 @@
-#import threading, thread
 import yaml
 import os
 import json
@@ -7,39 +6,8 @@ import time
 import itertools
 import basic_sentiment_analysis
 
-# Child: begins streaming tweets for LOCATION
-def child(location):
-  global locations
-  # put into twitter-api-friendly format
-  coordinates = [locations[location][0], locations[location][1]]
 
-
-  # Open API connection
-  global api
-  api = twitter.Api(locations[location][2],
-      locations[location][3],
-      locations[location][4],
-      locations[location][5])
-
-  # loop run with each new tweet
-  for line in api.GetStreamFilter(locations=coordinates):
-    if 'text' in line:
-      tweet = json.dumps(line["text"])
-      print "\n[location: " + location + "] tweet: " + tweet
-
-      #CRITICAL SECTION
-      global meter_lock
-      with meter_lock:
-        meter = update_meter(tweet)
-
-      file_name = location + '_meter.txt'
-      wr = open(file_name, 'w')
-      wr.write(meter)
-
-  return
-
-# Parent: creates children, assigning map location (& API keys) from YAML
-def parent():
+def main():
   global meter
   meter = 0
 
@@ -71,7 +39,7 @@ def parent():
 
     # loop run with each new tweet
     for line in api.GetStreamFilter(locations=coordinates):
-      if i >= 60:
+      if i >= 20:
         end_time = time.time()
         print "\nLocation: " + location + " processing time: " + str(end_time-start_time) + "for 20 tweets."
         break
@@ -100,4 +68,4 @@ def update_meter(tweet):
 
   return str(meter)
 
-parent()
+main()
